9c2208eb7bde810eac68815175e67ca5bb9d7960
a-garriga <134711795+a-garriga@users.noreply.github.com>
2024-04-15 16:00:03 -0400

    Update setup_LL_db.sql

--- setup/setup_LL_db.sql
+++ setup/setup_LL_db.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE IF NOT EXISTS LL_database;
 USE LL_database;
 
--- Create the CHAMPION table
+
 CREATE TABLE IF NOT EXISTS CHAMPION (
-    cname TEXT NOT NULL PRIMARY KEY,
+    cname VARCHAR(255) NOT NULL PRIMARY KEY,
     recommend_build_id INTEGER,
     recommended_skillorder_id INTEGER,
     recommended_runepage_id INTEGER,
@@ -12,42 +12,42 @@ CREATE TABLE IF NOT EXISTS CHAMPION (
     FOREIGN KEY (recommended_runepage_id) REFERENCES RUNE_PAGE(id)
 );
 
--- Create the BUILD table
+
 CREATE TABLE IF NOT EXISTS BUILD (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     game_count INTEGER,
     win_count INTEGER,
-    item1 TEXT,
-    item2 TEXT,
-    item3 TEXT,
-    cname TEXT,
+    item1 VARCHAR(255),
+    item2 VARCHAR(255),
+    item3 VARCHAR(255),
+    cname VARCHAR(255),
     FOREIGN KEY (cname) REFERENCES CHAMPION(cname) ON DELETE CASCADE
 );
 
--- Create the SKILL_ORDER table
+
 CREATE TABLE IF NOT EXISTS SKILL_ORDER (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     game_count INTEGER,
     win_count INTEGER,
-    skill1 TEXT,
-    skill2 TEXT,
-    skill3 TEXT,
-    cname TEXT,
+    skill1 VARCHAR(255),
+    skill2 VARCHAR(255),
+    skill3 VARCHAR(255),
+    cname VARCHAR(255),
     FOREIGN KEY (cname) REFERENCES CHAMPION(cname) ON DELETE CASCADE
 );
 
--- Create the RUNE_PAGE table
+
 CREATE TABLE IF NOT EXISTS RUNE_PAGE (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     game_count INTEGER,
     win_count INTEGER,
-    slot1 TEXT,
-    slot2 TEXT,
-    slot3 TEXT,
-    slot4 TEXT,
-    slot5 TEXT,
-    slot6 TEXT,
-    cname TEXT,
+    slot1 VARCHAR(255),
+    slot2 VARCHAR(255),
+    slot3 VARCHAR(255),
+    slot4 VARCHAR(255),
+    slot5 VARCHAR(255),
+    slot6 VARCHAR(255),
+    cname VARCHAR(255),
     FOREIGN KEY (cname) REFERENCES CHAMPION(cname) ON DELETE CASCADE
 );
 
