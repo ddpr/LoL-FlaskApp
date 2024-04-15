33cba13a0ea129f9df3fbf70d75274a2ad45e9fa
mbucataru <mbucatarugithub@gmail.com>
2024-04-15 12:26:09 -0400

    Create get_origin_matchlist()

--- datacollection/collection.py
+++ datacollection/collection.py
@@ -11,8 +11,15 @@ def api_key():
 
 api = LolWatcher(api_key())
 
-emerald_league = api.league.entries(region=REGION, queue="RANKED_SOLO_5x5", tier="EMERALD", division="IV")
-leagueIdOfCrawlStart = emerald_league[0]['summonerId']
-print(leagueIdOfCrawlStart)
-puuidOfCrawlStart = api.summoner.by_id(region=REGION, encrypted_summoner_id=leagueIdOfCrawlStart)
-print(puuidOfCrawlStart)
+# Returns array of matchIDs from the origin player
+def get_origin_matchlist():
+    emerald_league = api.league.entries(region=REGION, queue="RANKED_SOLO_5x5", tier="EMERALD", division="IV")
+    leagueIdOfCrawlStart = emerald_league[0]['summonerId']
+    print(leagueIdOfCrawlStart)
+    puuidOfCrawlStart = api.summoner.by_id(region=REGION, encrypted_summoner_id=leagueIdOfCrawlStart)['puuid']
+    print(puuidOfCrawlStart)
+    matchlist = api.match.matchlist_by_puuid(region=REGION, puuid=puuidOfCrawlStart)
+    return matchlist
+
+
+
