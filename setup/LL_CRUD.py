326ffc6d34ade5d3c64f3fc9973439d68da50fce
mbucataru <mbucatarugithub@gmail.com>
2024-04-15 20:43:29 -0400

    Add requests for parameters

--- setup/LL_CRUD.py
+++ setup/LL_CRUD.py
@@ -43,13 +43,14 @@ def delete_champion(cname):
     return run_query("DELETE FROM CHAMPION WHERE cname = %s", (cname,))
  
 # CRUD for BUILD
+# requested params: item1, item2, item3, cname
 def create_build(game_count, win_count, item1, item2, item3, cname):
     return run_query("INSERT INTO BUILD VALUES (%s, %s, %s, %s, %s, %s)",
                      (game_count, win_count, item1, item2, item3, cname))
- 
+# requested params: item1, item2, item3, cname
 def read_build(id):
     return run_query("SELECT * FROM BUILD WHERE id = %s", (id,), is_select=True)
- 
+# requested params: item1, item2, item3, cname, winLossIncrement (this will be a 0 or a 1 to note if you should add 1 to win_count)
 def update_build(id, game_count, win_count, item1, item2, item3, cname):
     return run_query("UPDATE BUILD SET game_count = %s, win_count = %s, item1 = %s, item2 = %s, item3 = %s WHERE id = %s",
                      (game_count, win_count, item1, item2, item3, id))
@@ -58,13 +59,14 @@ def delete_build(id):
     return run_query("DELETE FROM BUILD WHERE id = %s", (id,))
  
 # CRUD for SKILL_ORDER
+# requested params: skill1, skill2, skill3, cname
 def create_skill_order(game_count, win_count, skill1, skill2, skill3, cname):
     return run_query("INSERT INTO SKILL_ORDER VALUES (%s, %s, %s, %s, %s, %s)",
                      (game_count, win_count, skill1, skill2, skill3, cname))
- 
+# requested params: skill1, skill2, skill3, cname
 def read_skill_order(id):
     return run_query("SELECT * FROM SKILL_ORDER WHERE id = %s", (id,), is_select=True)
- 
+# requested params: skill1, skill2, skill3, cname, winLossIncrement (this will be a 0 or a 1 to note if you should add 1 to win_count)
 def update_skill_order(id, game_count, win_count, skill1, skill2, skill3, cname):
     return run_query("UPDATE SKILL_ORDER SET game_count = %s, win_count = %s, skill1 = %s, skill2 = %s, skill3 = %s WHERE id = %s",
                      (game_count, win_count, skill1, skill2, skill3, id))
@@ -73,13 +75,14 @@ def delete_skill_order(id):
     return run_query("DELETE FROM SKILL_ORDER WHERE id = %s", (id,))
  
 # CRUD for RUNE_PAGE
+# requested params: slot1, slot2, slot3, slot4, slot5, slot6, cname
 def create_rune_page(game_count, win_count, slot1, slot2, slot3, slot4, slot5, slot6, cname):
     return run_query("INSERT INTO RUNE_PAGE VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
                      (game_count, win_count, slot1, slot2, slot3, slot4, slot5, slot6, cname))
- 
+# requested params: slot1, slot2, slot3, slot4, slot5, slot6, cname
 def read_rune_page(id):
     return run_query("SELECT * FROM RUNE_PAGE WHERE id = %s", (id,), is_select=True)
- 
+# requested params: slot1, slot2, slot3, slot4, slot5, slot6, cname, winLossIncrement(this will be a 0 or a 1 to note if you should add 1 to win_count)
 def update_rune_page(id, game_count, win_count, slot1, slot2, slot3, slot4, slot5, slot6, cname):
     return run_query("UPDATE RUNE_PAGE SET game_count = %s, win_count = %s, slot1 = %s, slot2 = %s, slot3 = %s, slot4 = %s, slot5 = %s, slot6 = %s WHERE id = %s",
                      (game_count, win_count, slot1, slot2, slot3, slot4, slot5, slot6, id))
