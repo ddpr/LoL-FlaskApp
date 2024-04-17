77c0eaf7758b44e88301f6dcedb41d01d7ca23b7
a-garriga <134711795+a-garriga@users.noreply.github.com>
2024-04-17 03:14:58 -0400

    Update LL_CRUD.py
    edited to handle updates and new parameters for skill, build, and rune

--- setup/LL_CRUD.py
+++ setup/LL_CRUD.py
@@ -1,99 +1,162 @@
-import mysql.connector
-from mysql.connector import Error
+import pymysql
+from pymysql import err
 
 DATABASE_NAME = "LL_database"
 
 # Utility function to execute a query with error handling
-def run_query(query, parameters=(), is_select=False):
+def run_query(query, parameters=None, is_select=False):
+    result = None
     try:
-        conn = mysql.connector.connect(
-            host='localhost',        #potentially replace
-            database=DATABASE_NAME,
-            user='your_username',    # need to replace
-            password='your_password' #need to replace
+        conn = pymysql.connect(
+            host='localhost',          # Replace 
+            db=DATABASE_NAME,
+            user='your_username',      # Replace 
+            password='your_password',  # Replace 
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
         )
-        cursor = conn.cursor()
-        cursor.execute(query, parameters)
-        if is_select:
-            result = cursor.fetchall()
-            cursor.close()
-            conn.close()
-            return result
-        conn.commit()
-        last_row_id = cursor.lastrowid
-        cursor.close()
-        conn.close()
-        return last_row_id
-    except Error as e:
+        with conn.cursor() as cursor:
+            cursor.execute(query, parameters)
+            if is_select:
+                result = cursor.fetchall()
+            else:
+                conn.commit()
+                result = cursor.lastrowid
+    except err.MySQLError as e:
         print(f"SQL Error: {e}")
+    finally:
+        if conn:
+            conn.close()
+    return result
 
-# CRUD for CHAMPION
+# CRUD operations for the 'CHAMPION' table
 def create_champion(cname, recommend_build_id, recommended_skillorder_id, recommended_runepage_id):
-    return run_query("INSERT INTO CHAMPION VALUES (%s, %s, %s, %s)",
-                     (cname, recommend_build_id, recommended_skillorder_id, recommended_runepage_id))
- 
+    query = """
+    INSERT INTO CHAMPION (cname, recommend_build_id, recommended_skillorder_id, recommended_runepage_id)
+    VALUES (%s, %s, %s, %s)
+    """
+    return run_query(query, (cname, recommend_build_id, recommended_skillorder_id, recommended_runepage_id))
+
 def read_champion(cname):
-    return run_query("SELECT * FROM CHAMPION WHERE cname = %s", (cname,), is_select=True)
- 
+    query = "SELECT * FROM CHAMPION WHERE cname = %s"
+    return run_query(query, (cname,), is_select=True)
+
 def update_champion(cname, recommend_build_id, recommended_skillorder_id, recommended_runepage_id):
-    return run_query("UPDATE CHAMPION SET recommend_build_id = %s, recommended_skillorder_id = %s, recommended_runepage_id = %s WHERE cname = %s",
-                     (recommend_build_id, recommended_skillorder_id, recommended_runepage_id, cname))
- 
+    query = """
+    UPDATE CHAMPION SET recommend_build_id = %s, recommended_skillorder_id = %s, recommended_runepage_id = %s
+    WHERE cname = %s
+    """
+    return run_query(query, (recommend_build_id, recommended_skillorder_id, recommended_runepage_id, cname))
+
 def delete_champion(cname):
-    return run_query("DELETE FROM CHAMPION WHERE cname = %s", (cname,))
- 
-# CRUD for BUILD
+    query = "DELETE FROM CHAMPION WHERE cname = %s"
+    return run_query(query, (cname,))
+
+# CRUD operations for the 'BUILD' table
 def create_build(win_count, item1, item2, item3, cname):
-    return run_query("INSERT INTO BUILD VALUES (%s, %s, %s, %s, %s, %s)",
-                     (1, win_count, item1, item2, item3, cname))
+    query = """
+    INSERT INTO BUILD (game_count, win_count, item1, item2, item3, cname)
+    VALUES (1, %s, %s, %s, %s, %s)
+    """
+    return run_query(query, (win_count, item1, item2, item3, cname))
 
 def read_build(item1, item2, item3, cname):
-    return run_query("SELECT * FROM BUILD WHERE item1 = %s, item2 = %s, item3 = %s, cname = %s", (item1, item2, item3, cname), is_select=True)
+    query = """
+    SELECT * FROM BUILD WHERE item1 = %s AND item2 = %s AND item3 = %s AND cname = %s
+    """
+    return run_query(query, (item1, item2, item3, cname), is_select=True)
 
-# requested params: item1, item2, item3, cname, winLossIncrement (this will be a 0 or a 1 to note if you should add 1 to win_count)
-def update_build(id, game_count, win_count, item1, item2, item3, cname):
-    return run_query("UPDATE BUILD SET game_count = %s, win_count = %s, item1 = %s, item2 = %s, item3 = %s WHERE id = %s",
-                     (game_count, win_count, item1, item2, item3, id))
- 
-def delete_build(id):
-    return run_query("DELETE FROM BUILD WHERE id = %s", (id,))
- 
-# CRUD for SKILL_ORDER
+def update_build(winOrLoss, item1, item2, item3, cname):
+    getWinsGamesQ = """
+    SELECT win_count, game_count FROM BUILD WHERE item1 = %s AND item2 = %s AND item3 = %s AND cname = %s
+    """
+    result = run_query(getWinsGamesQ, (item1, item2, item3, cname), is_select=True)
+    if result:
+        current_wins, current_games = result[0]['win_count'], result[0]['game_count']
+        query = """
+        UPDATE BUILD SET game_count = %s, win_count = %s WHERE item1 = %s AND item2 = %s AND item3 = %s AND cname = %s
+        """
+        return run_query(query, (current_games + 1, current_wins + winOrLoss, item1, item2, item3, cname))
+
+def delete_build(item1, item2, item3, cname):
+    query = """
+    DELETE FROM BUILD WHERE item1 = %s AND item2 = %s AND item3 = %s AND cname = %s
+    """
+    return run_query(query, (item1, item2, item3, cname))
+
+# CRUD operations for the 'SKILL_ORDER' table
 def create_skill_order(win_count, skill1, skill2, skill3, cname):
-    return run_query("INSERT INTO SKILL_ORDER VALUES (%s, %s, %s, %s, %s, %s)",
-                     (1, win_count, skill1, skill2, skill3, cname))
+    query = """
+    INSERT INTO SKILL_ORDER (game_count, win_count, skill1, skill2, skill3, cname)
+    VALUES (1, %s, %s, %s, %s, %s)
+    """
+    return run_query(query, (win_count, skill1, skill2, skill3, cname))
 
 def read_skill_order(skill1, skill2, skill3, cname):
-    return run_query("SELECT * FROM SKILL_ORDER WHERE skill1 = %s, skill2 = %s, skill3 = %s, cname = %s", (skill1, skill2, skill3, cname), is_select=True)
+    query = """
+    SELECT * FROM SKILL_ORDER WHERE skill1 = %s AND skill2 = %s AND skill3 = %s AND cname = %s
+    """
+    return run_query(query, (skill1, skill2, skill3, cname), is_select=True)
 
-# requested params: skill1, skill2, skill3, cname, winLossIncrement (this will be a 0 or a 1 to note if you should add 1 to win_count)
-def update_skill_order(id, game_count, win_count, skill1, skill2, skill3, cname):
-    return run_query("UPDATE SKILL_ORDER SET game_count = %s, win_count = %s, skill1 = %s, skill2 = %s, skill3 = %s WHERE id = %s",
-                     (game_count, win_count, skill1, skill2, skill3, id))
- 
-def delete_skill_order(id):
-    return run_query("DELETE FROM SKILL_ORDER WHERE id = %s", (id,))
- 
-# CRUD for RUNE_PAGE
+def update_skill_order(winOrLoss, skill1, skill2, skill3, cname):
+    getWinsGamesQ = """
+    SELECT win_count, game_count FROM SKILL_ORDER WHERE skill1 = %s AND skill2 = %s AND skill3 = %s AND cname = %s
+    """
+    result = run_query(getWinsGamesQ, (skill1, skill2, skill3, cname), is_select=True)
+    if result:
+        current_wins, current_games = result[0]['win_count'], result[0]['game_count']
+        query = """
+        UPDATE SKILL_ORDER SET game_count = %s, win_count = %s WHERE skill1 = %s AND skill2 = %s AND skill3 = %s AND cname = %s
+        """
+        return run_query(query, (current_games + 1, current_wins + winOrLoss, skill1, skill2, skill3, cname))
+
+def delete_skill_order(skill1, skill2, skill3, cname):
+    query = """
+    DELETE FROM SKILL_ORDER WHERE skill1 = %s AND skill2 = %s AND skill3 = %s AND cname = %s
+    """
+    return run_query(query, (skill1, skill2, skill3, cname))
+
+# CRUD operations for the 'RUNE_PAGE' table
 def create_rune_page(win_count, slot1, slot2, slot3, slot4, slot5, slot6, cname):
-    return run_query("INSERT INTO RUNE_PAGE VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                     (1, win_count, slot1, slot2, slot3, slot4, slot5, slot6, cname))
+    query = """
+    INSERT INTO RUNE_PAGE (game_count, win_count, slot1, slot2, slot3, slot4, slot5, slot6, cname)
+    VALUES (1, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    return run_query(query, (win_count, slot1, slot2, slot3, slot4, slot5, slot6, cname))
 
 def read_rune_page(slot1, slot2, slot3, slot4, slot5, slot6, cname):
-    return run_query("SELECT * FROM RUNE_PAGE WHERE slot1 = %s, slot2 = %s, slot3 = %s, slot4 = %s, slot5 = %s, slot6 = %s, cname = %s", (slot1, slot2, slot3, slot4, slot5, slot6, cname), is_select=True)
+    query = """
+    SELECT * FROM RUNE_PAGE WHERE slot1 = %s AND slot2 = %s AND slot3 = %s AND slot4 = %s AND slot5 = %s AND slot6 = %s AND cname = %s
+    """
+    return run_query(query, (slot1, slot2, slot3, slot4, slot5, slot6, cname), is_select=True)
 
-# requested params: slot1, slot2, slot3, slot4, slot5, slot6, cname, winLossIncrement(this will be a 0 or a 1 to note if you should add 1 to win_count)
-def update_rune_page(id, game_count, win_count, slot1, slot2, slot3, slot4, slot5, slot6, cname):
-    return run_query("UPDATE RUNE_PAGE SET game_count = %s, win_count = %s, slot1 = %s, slot2 = %s, slot3 = %s, slot4 = %s, slot5 = %s, slot6 = %s WHERE id = %s",
-                     (game_count, win_count, slot1, slot2, slot3, slot4, slot5, slot6, id))
- 
-def delete_rune_page(id):
-    return run_query("DELETE FROM RUNE_PAGE WHERE id = %s", (id,))
-#CRUD for MATCH
+def update_rune_page(winOrLoss, slot1, slot2, slot3, slot4, slot5, slot6, cname):
+    getWinsGamesQ = """
+    SELECT win_count, game_count FROM RUNE_PAGE WHERE slot1 = %s AND slot2 = %s AND slot3 = %s AND slot4 = %s AND slot5 = %s AND slot6 = %s AND cname = %s
+    """
+    result = run_query(getWinsGamesQ, (slot1, slot2, slot3, slot4, slot5, slot6, cname), is_select=True)
+    if result:
+        current_wins, current_games = result[0]['win_count'], result[0]['game_count']
+        query = """
+        UPDATE RUNE_PAGE SET game_count = %s, win_count = %s WHERE slot1 = %s, slot2 = %s, slot3 = %s, slot4 = %s, slot5 = %s, slot6 = %s, cname = %s
+        """
+        return run_query(query, (current_games + 1, current_wins + winOrLoss, slot1, slot2, slot3, slot4, slot5, slot6, cname))
+
+def delete_rune_page(slot1, slot2, slot3, slot4, slot5, slot6, cname):
+    query = """
+    DELETE FROM RUNE_PAGE WHERE slot1 = %s, slot2 = %s, slot3 = %s, slot4 = %s, slot5 = %s, slot6 = %s, cname = %s
+    """
+    return run_query(query, (slot1, slot2, slot3, slot4, slot5, slot6, cname))
+
+# CRUD operations for the 'MATCHES' table
 def create_match(id):
-    return run_query("INSERT INTO MATCH VALUES (%s)" , (id,));
-def read_match(id):
-  return run_query("SELECT * FROM MATCH WHERE ID = %s" (id,), is_select=True)
+    query = "INSERT INTO MATCHES (id) VALUES (%s)"
+    return run_query(query, (id,))
+    
+def read_all_matches():
+    query = "SELECT * FROM MATCHES"
+    return run_query(query, (), is_select=True)
+    
 def delete_match(id):
-    return run_query("DELETE FROM MATCH WHERE id = %s", (id,))
- 
+    query = "DELETE FROM MATCHES WHERE id = %s"
+    return run_query(query, (id,))
